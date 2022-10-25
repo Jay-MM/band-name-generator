85f9ceca4d00604b1a8baff1288e2e49930ec764
Jesus M <marquez.jay444@gmail.com>
2022-10-24 21:22:23 -0500

    can generate band names and colors

---
+++ index.js
@@ -0,0 +1,8 @@
+import {generateSlug } from 'random-word-slugs'
+import chalkAnimation from "chalk-animation"
+
+const length = parseInt(process.argv[2])
+
+const bandName = generateSlug(length, {format: "title"})
+
+chalkAnimation.rainbow(bandName)
